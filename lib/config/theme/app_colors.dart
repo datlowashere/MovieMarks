81b56e205dbaa4ed7cfafcadcefb2c60b9b6ad2f
Đạt <datlowashere@gmail.com>
2024-12-17 13:55:06 +0700

    initial project

---
+++ lib/config/theme/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const shamrockGreen = Color(0xff0C9A61);
+  static const cultured = Color(0xffF7F7F7);
+  static const platinum = Color(0xffE2E2E2);
+}
---
+++ lib/constants/app_contants.dart
@@ -0,0 +1,4 @@
+class AppConstants{
+  static const movieUrl = "https://api.themoviedb.org/3/";
+  static const imageUrl = "https://image.tmdb.org/t/p/original";
+}
